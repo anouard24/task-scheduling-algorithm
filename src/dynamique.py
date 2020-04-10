b3000b104c26636e3de6f9404ced8444806cfba4
anouard24 <anouar.fadili.3@gmail.com>
2020-04-10 18:54:10 +0100

    Delete unused function dynamique_all_f in dynamique.py

--- src/dynamique.py
+++ src/dynamique.py
@@ -54,36 +54,3 @@ def dynamique_all(p,w,d):
 		temp_dictionary = dict()
 
 	return dictionary.get(indices)[:2]
-
-def dynamique_all_f(p,w,d):
-	n = len(p)
-	indices = tuple(range(n))
-	dictionary = temp_dictionary = {}
-	all_sol_dict = {}
-	dictionary[()] = (0,[[]],0)
-	for n_i in range(1,n+1):
-		for current_combin in combinations(indices,n_i):
-			min_penalty = float("inf")
-			best_sequence = []
-			cout = 0
-			for k,r in enumerate(combinations(current_combin,n_i-1)):
-				j = current_combin[n_i-k-1]
-				fa,fr,cout = dictionary.get(r)
-				cout += p[j]
-				ff = fa + max(0,cout-d[j])*w[j]
-				if ff <= min_penalty:
-					if ff < min_penalty:
-						all_sol = []
-						min_penalty = ff
-						best_sequence = fr + [j]
-					for old_sol in fr:
-						all_sol.append(old_sol+[j])
-			# temp_dictionary[current_combin] = (min_penalty,best_sequence,cout)
-			temp_dictionary[current_combin] = (min_penalty,all_sol,cout)
-		all_sol_dict.update(temp_dictionary)
-		dictionary = temp_dictionary
-		temp_dictionary = dict()
-
-	list_all_f = sorted(all_sol_dict.values(),key=lambda s:len(s[1][0]))
-	return dictionary.get(indices)[:2],list_all_f
-
