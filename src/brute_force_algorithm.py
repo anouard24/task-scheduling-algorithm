dac716fc61cb94f7ba40ce59e02f5fd4126caf1e
anouard24 <anouar.fadili.3@gmail.com>
2020-04-10 03:33:25 +0100

    Refactor brute_force_algorithm.py
    add docstrings and respect PEP8

--- src/brute_force_algorithm.py
+++ src/brute_force_algorithm.py
@@ -1,24 +1,55 @@
+"""
+Module provides functions to solve the task scheduling problem
+in naive method (Brute force search)
+Time complexity: O(N!)
+Space complexity: O(N)
+"""
+
 from itertools import permutations
 
 
-def fonction_objective(pwd,liste):
-	p,w,d = pwd
-	s = c = 0
-	for x in liste:
-		c += p[x]
-		s += max(0,c-d[x])*w[x]
-	return s
+def penalty_of_tasks(pwd, tasks):
+    """
+    Function to calculate cost of a given order of task
+    params:
+        pwd: multi-dimensional list [P, W, D] that contain:
+            - P Execution time
+            - W Weight penalty
+            - D Date limit
+        tasks: task indices
+    """
+
+    execution_time, weight_penalty, date_limit = pwd
+
+    sum_cost = cost = 0
+
+    for task in tasks:
+        cost += execution_time[task]
+        sum_cost += max(0, cost - date_limit[task]) * weight_penalty[task]
+
+    return sum_cost
 
 
-def resolve_n_fact(pwdx):
-	n = len(pwdx[0])
-	indices = range(n)
-	l_best = []
-	p_best = float("inf")
-	for permut in permutations(indices):
-		p = fonction_objective(pwdx,list(permut))			
-		if p < p_best:
-			p_best = p
-			l_best = list(permut)
-	return l_best,p_best
+def resolve_brute_force(pwd):
+    """
+    Function to generate and test all possible permutations of tasks
+    and return the last best solution with the minimum possible penalty
+    params:
+        pwd: multi-dimensional list [P, W, D] that contain:
+            - P Execution time
+            - W Weight penalty
+            - D Date limit
+    """
+    tasks_number = len(pwd[0])
+    indexes = range(tasks_number)
 
+    best_order_tasks = []
+    min_penalty = float("inf")
+
+    for tasks in permutations(indexes):
+        current_penalty = penalty_of_tasks(pwd, list(tasks))
+        if current_penalty < min_penalty:
+            min_penalty = current_penalty
+            best_order_tasks = list(tasks)
+
+    return best_order_tasks, min_penalty
