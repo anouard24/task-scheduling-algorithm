3f14eb640a4f209110500ff09d3d809c10543b7e
anouard24 <anouar.fadili.3@gmail.com>
2020-04-11 02:39:13 +0100

    Refactor and remove linter warnings
    Remove linter warnings by refactor and add docstrings to module and functions

--- src/dynamique.py
+++ src/dynamique.py
@@ -1,56 +1,71 @@
+"""
+Module provides functions to solve the task scheduling problem
+using dynamic programming approach
+Time complexity: O(N*(2^N))
+"""
+
 from itertools import combinations
 
-def dynamique(p,w,d):
-	n = len(p)
-	indices = tuple(range(n))
-	dictionary = temp_dictionary = {}
-	dictionary[()] = (0,[],0)
-	for n_i in range(1,n+1):
-		for current_combin in combinations(indices,n_i):
-			min_penalty = float("inf")
-			best_sequence = []
-			cout = 0
-			for k,r in enumerate(combinations(current_combin,n_i-1)):
-				j = current_combin[n_i-k-1]
-				fa,fr,cout = dictionary.get(r)
-				cout += p[j]
-				ff = fa + max(0,cout-d[j])*w[j]
-				if ff < min_penalty:
-					min_penalty = ff
-					best_sequence = fr + [j]
-					if ff==0:
-						break
-			temp_dictionary[current_combin] = (min_penalty,best_sequence,cout)
-		dictionary = temp_dictionary
-		temp_dictionary = dict()
 
-	return dictionary.get(indices)[:2]
+def dynamique(p, w, d):
+    """
+    Function that return one of best possible ordred tasks
+    Space complexity: O(N)
+    """
+    n = len(p)
+    indices = tuple(range(n))
+    dictionary = temp_dictionary = {}
+    dictionary[()] = (0, [], 0)
+    for n_i in range(1, n + 1):
+        for current_tasks in combinations(indices, n_i):
+            min_penalty = float("inf")
+            best_sequence = []
+            cost = 0
+            for k, r in enumerate(combinations(current_tasks, n_i - 1)):
+                j = current_tasks[n_i - k - 1]
+                fa, fr, cost = dictionary.get(r)
+                cost += p[j]
+                ff = fa + max(0, cost - d[j]) * w[j]
+                if ff < min_penalty:
+                    min_penalty = ff
+                    best_sequence = fr + [j]
+                    if ff == 0:
+                        break
+            temp_dictionary[current_tasks] = (min_penalty, best_sequence, cost)
+        dictionary = temp_dictionary
+        temp_dictionary = dict()
 
-def dynamique_all(p,w,d):
-	n = len(p)
-	indices = tuple(range(n))
-	dictionary = temp_dictionary = {}
-	dictionary[()] = (0,[[]],0)
-	for n_i in range(1,n+1):
-		for current_combin in combinations(indices,n_i):
-			min_penalty = float("inf")
-			best_sequence = []
-			cout = 0
-			for k,r in enumerate(combinations(current_combin,n_i-1)):
-				j = current_combin[n_i-k-1]
-				fa,fr,cout = dictionary.get(r)
-				cout += p[j]
-				ff = fa + max(0,cout-d[j])*w[j]
-				if ff <= min_penalty:
-					if ff < min_penalty:
-						all_sol = []
-						min_penalty = ff
-						best_sequence = fr + [j]
-					for old_sol in fr:
-						all_sol.append(old_sol+[j])
-			# temp_dictionary[current_combin] = (min_penalty,best_sequence,cout)
-			temp_dictionary[current_combin] = (min_penalty,all_sol,cout)
-		dictionary = temp_dictionary
-		temp_dictionary = dict()
+    return dictionary.get(indices)[:2]
 
-	return dictionary.get(indices)[:2]
+
+def dynamique_all(p, w, d):
+    """
+    Function that return all possible best ordered tasks
+    Space complexity: O(N!)
+    """
+    n = len(p)
+    indices = tuple(range(n))
+    dictionary = temp_dictionary = {}
+    dictionary[()] = (0, [[]], 0)
+
+    for n_i in range(1, n + 1):
+        for current_tasks in combinations(indices, n_i):
+            min_penalty = float("inf")
+            all_sol = []
+            cost = 0
+            for k, r in enumerate(combinations(current_tasks, n_i - 1)):
+                j = current_tasks[n_i - k - 1]
+                fa, fr, cost = dictionary.get(r, None)
+                cost += p[j]
+                ff = fa + max(0, cost - d[j]) * w[j]
+                if ff <= min_penalty:
+                    if ff < min_penalty:
+                        all_sol = []
+                        min_penalty = ff
+                    for old_sol in fr:
+                        all_sol.append(old_sol + [j])
+            temp_dictionary[current_tasks] = (min_penalty, all_sol, cost)
+        dictionary = temp_dictionary
+        temp_dictionary = dict()
+
+    return dictionary.get(indices)[:2]
